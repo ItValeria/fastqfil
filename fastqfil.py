725d16f24dea2efab4fc71e0ed60753433aae931
ItValeria <aopex@mail.ru>
2025-03-01 23:52:45 +0300

    HW15

--- fastqfil.py
+++ fastqfil.py
@@ -1,89 +1,127 @@
-from additional_scripts.filtration_fastq import filt_fastq
 import os
-from additional_scripts.procedures_with_dna_and_rna import (
-    check_of_procedure,
-    complement,
-    reverse,
-    reverse_complement,
-    transcribe,
-    type_of_chain,
-)
-
-"""Accepts dictionaries with the sequence name,
-sequence and its quality of reading,
-filters according to the specified parameters
-of the percentage of GC nucleotides,
-the length of the sequences and the average quality
-of reading. Returns a filtered dictionary"""
+from abc import ABC, abstractmethod
+from Bio import SeqIO
+from Bio.SeqUtils import gc_fraction
 
 
-def filter_fastq(
-    input_fastq,
-    output_fastq,
-    gc_bounds=(0, 100),
-    length_bounds=(0, 2**32),
-    quality_threshold=0,
-):
+#### Ex 1
+
+
+class InvalidBiologicalSequence(ValueError):
+    pass
+ 
+class BiologicalSequence(ABC):
+    """Abstract class for biological sequences"""
+
+    def __init__(self, sequence: str):
+        if not self._check_alphabet(sequence):
+            raise InvalidBiologicalSequence(f"Wrong alphapet of biological sequence: {sequence}")
+        self.sequence = sequence
+
+    def __len__(self):
+        return len(self.sequence)
+
+    def __getitem__(self, index):
+        return self.sequence[index]
+
+    def __str__(self):
+        return self.sequence
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.sequence}')"
+
+    @abstractmethod
+    def _check_alphabet(self, sequence: str) -> bool:
+        """Cheks symbols of biological sequence alphabet"""
+        pass
+
+
+class NucleicAcidSequence(BiologicalSequence):
+    """Class for nucleic acid sequences"""
+
+    complements = {}
+    
+    def reverse(self):
+        """Returns reversed sequence"""
+        return self.__class__(self.sequence[::-1])
+
+    def complement(self):
+        """Returns complementary sequence"""
+        return self.__class__("".join(self.complements[n] for n in self.sequence))
+
+    def reverse_complement(self):
+        """Returns reversed and complementary sequence"""
+        return self.complement().reverse()
+
+
+class DNASequence(NucleicAcidSequence):
+    """Class for DNA sequences"""
+
+    complements = {"A": "T", "T": "A", "G": "C", "C": "G"}
+
+    def _check_alphabet(self, sequence: str) -> bool:
+        return all(n in "ATGC" for n in sequence)
+
+    def transcribe(self):
+        """Transcribes DNA to RNA"""
+        return RNASequence(self.sequence.replace("T", "U"))
+
+    
+class RNASequence(NucleicAcidSequence):
+    """Class for RNA sequences"""
+
+    complements = {"A": "U", "U": "A", "G": "C", "C": "G"}
+
+    def _check_alphabet(self, sequence: str) -> bool:
+        return all(n in "AUGC" for n in sequence)
+
+    
+class AminoAcidSequence(BiologicalSequence):
+    """Class for amino acid sequences"""
+
+    valid_amino_acids = "ACDEFGHIKLMNPQRSTVWY"
+
+    def _check_alphabet(self, sequence: str) -> bool:
+        return all(aa in self.valid_amino_acids for aa in sequence)
+
+    def amino_acid_composition(self):
+        """Returns dictionary with count of each amino acid"""
+        return {aa: self.sequence.count(aa) for aa in set(self.sequence)}
+    
+    
+#### Ex 2
+
+
+def filter_fastq(input_fastq, output_fastq, gc_bounds=(0, 100), length_bounds=(0, 2**32), quality_threshold=0):
+    """Filtrates FastQ-file by length, GC-count and quality"""
+    
     if not os.path.isdir("filtered"):
         os.mkdir("filtered")
     if isinstance(gc_bounds, int):
         gc_bounds = (0, gc_bounds)
     if isinstance(length_bounds, int):
         length_bounds = (0, length_bounds)
-    with open(input_fastq, "r") as read_fastq, open(
-        os.path.join("filtered", output_fastq), "w"
-    ) as write_fastq:
-        name, sequence, commentary, quality = "", "", "", ""
-        count_of_line = 0
-        # Reading 4 lines of the same sequence
-        for line in read_fastq:
-            if count_of_line == 0:
-                name = line
-                count_of_line += 1
-            elif count_of_line == 1:
-                sequence = line
-                count_of_line += 1
-            elif count_of_line == 2:
-                commentary = line
-                count_of_line += 1
-            elif count_of_line == 3:
-                quality = line
-                count_of_line = 0
-                # Checking the length, GC content and reading quality of nucleotide chains
-                if filt_fastq(
-                    name, sequence, quality, gc_bounds, length_bounds, quality_threshold
-                ):
-                    # Writing the verified sequences to an external file
-                    write_fastq.write(name)
-                    write_fastq.write(sequence)
-                    write_fastq.write(commentary)
-                    write_fastq.write(quality)
-                name, sequence, commentary, quality = "", "", "", ""
+   
+    with open(input_fastq, "r") as read_fastq, open(os.path.join("filtered", output_fastq), "w") as write_fastq:
+       
+        # Reading fastq-file
+        for record in SeqIO.parse(read_fastq, "fastq"):
+            sequence = record.seq
+            quality = record.letter_annotations["phred_quality"]
+           
+            # checks length
+            if not (length_bounds[0] <= len(sequence) <= length_bounds[1]):
+                continue
 
+            # checks GC-count
+            gc_content = gc_fraction(sequence)
+            if not (gc_bounds[0] <= gc_content*100 <= gc_bounds[1]):
+                continue
 
-# Receives the nucleotide chains and the procedure
-# Checks the correctness of the input
-# Returns the changed circuits
-def run_dna_rna_tools(*nucleotide_chains_and_procedure):
-    procedures = {
-        "transcribe": transcribe,
-        "reverse": reverse,
-        "complement": complement,
-        "reverse_complement": reverse_complement,
-    }
-    nucleotide_chains = []
-    procedure_name = nucleotide_chains_and_procedure[-1]
-    if not check_of_procedure(procedure_name):
-        return "ERROR: The requested procedure does not exist"
-    for chain in nucleotide_chains_and_procedure[:-1]:
-        if type_of_chain(chain) != "error":
-            nucleotide_chains += [chain]
-        else:
-            return f'ERROR: "{chain}" is not RNA or DNA'
-    if len(nucleotide_chains) == 1:
-        return procedures[procedure_name](nucleotide_chains[0])
-    else:
-        result_chains = []
-        for chain in nucleotide_chains:
-            result_chains += [procedures[procedure_name](chain)]
-        return result_chains
+            # checks quality
+            avg_quality = sum(quality) / len(quality)
+            if avg_quality < quality_threshold:
+                continue
+
+            # Records output file
+            SeqIO.write(record, write_fastq, "fastq")
